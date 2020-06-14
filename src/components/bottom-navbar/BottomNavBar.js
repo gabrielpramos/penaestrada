a3a899d4152011480bf92212edf0424a85fa1f0c
Gabriel Ramos <gabriel.ramos@liferay.com>
2020-06-13 22:18:42 -0300

    [Project] Create bottom navbar

---
+++ src/components/bottom-navbar/BottomNavBar.js
@@ -0,0 +1,37 @@
+import React, { useState } from "react";
+import { BottomNavigation, BottomNavigationAction } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import MapIcon from "@material-ui/icons/Map";
+import ChatIcon from "@material-ui/icons/Chat";
+
+import "./BottomNavBar.css";
+
+const BottomNavBar = () => {
+  const [value, setValue] = useState();
+  const classes = makeStyles({
+    root: {
+      width: 500,
+    },
+  })();
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.root}
+    >
+      <BottomNavigationAction
+        label="Saúde"
+        icon={<FavoriteIcon className="icon" />}
+      />
+      <BottomNavigationAction label="Mapa" icon={<MapIcon />} />
+      <BottomNavigationAction label="Conversas" icon={<ChatIcon />} />
+    </BottomNavigation>
+  );
+};
+
+export default BottomNavBar;
